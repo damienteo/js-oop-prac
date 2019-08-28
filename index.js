a3ee587eb8de6d4458606b3268a8031b72917459
damienteo <ksteo89@gmail.com>
2019-08-28 10:37:40 +0800

    Added stopwatch function

--- index.js
+++ index.js
@@ -75,3 +75,37 @@ console.log("keys", keys);
 if ("radius" in circle) console.log("circle has a radius");
 
 // delete anotherCircle.location;
+
+function Stopwatch() {
+  let startTime,
+    endTime,
+    running,
+    duration = 0;
+
+  this.start = () => {
+    if (running) throw new Error("Stopwatch has already started.");
+    running = true;
+    startTime = new Date();
+  };
+
+  this.stop = () => {
+    if (!running) throw new Error("Stopwatch has not started.");
+    running = false;
+    endTime = new Date();
+    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+    duration += seconds;
+  };
+
+  this.reset = () => {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  };
+
+  Object.defineProperty(this, "duration", {
+    get: function() {
+      return duration;
+    }
+  });
+}
