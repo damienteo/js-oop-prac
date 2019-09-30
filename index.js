cfbae5e2b64b7460ab2534c4dbc854dc48321e1f
damienteo <ksteo89@gmail.com>
2019-09-30 16:50:07 +0800

    Backing up files

--- index.js
+++ index.js
@@ -76,36 +76,103 @@ if ("radius" in circle) console.log("circle has a radius");
 
 // delete anotherCircle.location;
 
+// function Stopwatch() {
+//   let startTime,
+//     endTime,
+//     running,
+//     duration = 0;
+
+//   this.start = () => {
+//     if (running) throw new Error("Stopwatch has already started.");
+//     running = true;
+//     startTime = new Date();
+//   };
+
+//   this.stop = () => {
+//     if (!running) throw new Error("Stopwatch has not started.");
+//     running = false;
+//     endTime = new Date();
+//     const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+//     duration += seconds;
+//   };
+
+//   this.reset = () => {
+//     startTime = null;
+//     endTime = null;
+//     running = false;
+//     duration = 0;
+//   };
+
+//   Object.defineProperty(this, "duration", {
+//     get: function() {
+//       return duration;
+//     }
+//   });
+// }
+
 function Stopwatch() {
   let startTime,
     endTime,
     running,
     duration = 0;
 
-  this.start = () => {
-    if (running) throw new Error("Stopwatch has already started.");
-    running = true;
-    startTime = new Date();
-  };
+  Object.defineProperty(this, "startTime", {
+    get: function() {
+      return startTime;
+    }
+  });
 
-  this.stop = () => {
-    if (!running) throw new Error("Stopwatch has not started.");
-    running = false;
-    endTime = new Date();
-    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-    duration += seconds;
-  };
+  Object.defineProperty(this, "endTime", {
+    get: function() {
+      return endTime;
+    }
+  });
 
-  this.reset = () => {
-    startTime = null;
-    endTime = null;
-    running = false;
-    duration = 0;
-  };
+  Object.defineProperty(this, "running", {
+    get: function() {
+      return running;
+    }
+  });
 
   Object.defineProperty(this, "duration", {
     get: function() {
       return duration;
+    },
+    set: function(value) {
+      duration = value;
     }
   });
 }
+
+Stopwatch.prototype.start = () => {
+  if (this.running) throw new Error("Stopwatch has already started.");
+  this.running = true;
+  this.startTime = new Date();
+};
+
+Stopwatch.prototype.stop = () => {
+  if (!this.running) throw new Error("Stopwatch has not started.");
+  this.running = false;
+  this.endTime = new Date();
+  const seconds = (this.endTime.getTime() - this.startTime.getTime()) / 1000;
+  this.duration += seconds;
+};
+
+Stopwatch.prototype.reset = () => {
+  this.startTime = null;
+  this.endTime = null;
+  this.running = false;
+  this.duration = 0;
+};
+
+let person = { name: "name" };
+let objectBase = Object.getPrototypeOf(person);
+let objectProperties = Object.getOwnPropertyDescriptor(objectBase, "toString");
+
+console.log(objectProperties);
+
+Object.defineProperty(person, "name", {
+  writable: false,
+  enumerable: false,
+  configurable: false
+});
